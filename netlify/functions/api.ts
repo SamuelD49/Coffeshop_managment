7b6cd75bc6417c2adb41d8da64ecd84b3c32713e
Samuel <samuedereje61l@gmail.com>
2026-05-17 23:31:14 +0300

    Add Netlify deployment configuration

---
+++ netlify/functions/api.ts
@@ -0,0 +1,27 @@
+import serverless from "serverless-http";
+import { app } from "../../src/app";
+import { runMigrations } from "../../src/lib/db";
+
+let _initPromise: Promise<void> | null = null;
+function ensureInit(): Promise<void> {
+  if (!_initPromise) {
+    _initPromise = runMigrations().catch((err) => {
+      // If migrations fail, surface the error and let the next request retry.
+      _initPromise = null;
+      throw err;
+    });
+  }
+  return _initPromise;
+}
+
+// We add a middleware at the beginning to ensure initialization
+app.use(async (req, res, next) => {
+  try {
+    await ensureInit();
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+export const handler = serverless(app);
